c300c57dd529576385c799ad86e669f6c30900de
Kristina Fout <64440239+krisf01@users.noreply.github.com>
2022-02-18 17:08:56 -0800

    Update ShortyResty.go

--- ShortyResty.go
+++ ShortyResty.go
@@ -1,5 +1,5 @@
 package main
- 
+
 import (
    "log"
    "math/rand" // for rand
@@ -7,8 +7,11 @@ import (
    "encoding/json"
    "strings"
    "net/url"
+   "testing" // unit testing
+   "net/http/httptest" // unit testing
 )
  
+
 // JSON response for shortened URLs
 type jsonResponse struct {
    ShortURL string `json:"short_url"`
@@ -16,86 +19,111 @@ type jsonResponse struct {
  
 // JSON redirect for long URLs
 type jsonRedirect struct {
-   Long_url string `json:"url"`
+   LongURL string `json:"url"`
 }
- 
+
+// Map
 var pathToUrls map[string]jsonRedirect
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
  
-// function for the shorten function to shorten URL and contain an ID with random string of eight
+// Function to shorten the U              RL and contain an ID with random length of eight
 func string_eight() string {
-   // random characters for shortened URLs
+	// Characters for the ID
+	var variables = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+
+	// Randomizing the characters into length of eight
    var id string = ""
    for i := 0; i < 8; i++ {
-       id += string(chars[rand.Intn(len(chars))])
+       id += string(variables[rand.Intn(len(variables))])
    }
-   // returning random id char
+
+   // Returning the random variable ID
    return id
 }
  
 // Redirect function handles the GET requests and will redirect the user
 func redirect(writer http.ResponseWriter, req *http.Request) {
 
-   // leaving id of the URL only
+   // Leaving id of the URL only
    var id string = strings.TrimPrefix(req.RequestURI, "/") 
-   if _, ok := pathToUrls[id]; ok {
-	   http.Redirect(writer, req, pathToUrls[id].Long_url, http.StatusFound)
-   }
+
+   // Checking whether or not ID is within the map. Redirects the user, otherwise error
+   if _, key := pathToUrls[id]; key {
+		http.Redirect(writer, req, pathToUrls[id].LongURL, http.StatusFound)
+		return
+	} else {
+		writer.Write([]byte(req.RequestURI + ""))
+        return
+	}
 }
  
-// Shorten long URL function. Uses a map for redirect endpoint
+// Shorten long URL function. Uses a map for redirect endpoint.
 func shorten(writer http.ResponseWriter, req *http.Request){
 
-	// header
-	writer.Header().Set( "","application/json")
+	// Label HTTP header and request the header to be returned
+	writer.Header().Set("POST Request Content Type: ", "application/json")
 	writer.WriteHeader(http.StatusCreated)
 
-	// decoding the jsn redirect for long URLs
-	var longUrl jsonRedirect 
-	json.NewDecoder(req.Body).Decode(&longUrl)
+	// Decode the JSON redirect for long URLs. `json:"url"`
+	var long_Url jsonRedirect 
+	json.NewDecoder(req.Body).Decode(&long_Url)
 
-	// checking if URL is valid
-	_, err := url.ParseRequestURI(longUrl.Long_url)
+	// Checking if the provided URL is valid
+	_, err := url.ParseRequestURI(long_Url.LongURL)
 	if err != nil {
-		http.Error(writer, "Error, not valid!", http.StatusBadRequest)  
+		http.Error(writer, "Error, URL is not valid!", http.StatusBadRequest)  
 		return
 	}
 
-	// variable declaration for string and if it exists comparison
-	var id string
-	var exists bool = true
-
 	// While id exists in the map, it will generate random IDs
+	var exists bool = true
+	var id string
+
 	for exists {
 		id = string_eight()
 		if _, key := pathToUrls[id]; !key {
 			exists = false
 		}
 	}
+
 	// Adding the id of the long URL to the map
-	pathToUrls[id] = longUrl  
+	pathToUrls[id] = long_Url  
 
-	// creating the shortened url
-	var newUrl string
-   	newUrl = "http://127.0.0.1:8080/" + (string(id))
-	shortUrl := jsonResponse{newUrl}
+	// Creating the shortened url
+	var new_Url string
+   	new_Url = "http://127.0.0.1:8080/" + id
+	short_Url := jsonResponse{new_Url}
 
-	// creating JSON response so it can be sent
-	json.NewEncoder(writer).Encode(shortUrl) 
+	// Creating JSON response so it can be sent
+	json.NewEncoder(writer).Encode(short_Url) 
 }
 
-// starting web server within the main function
+// Starting web server within the main function
 func main() {
-   // creating the URL map
+   // Creating the URL map
    pathToUrls = make (map[string]jsonRedirect)
 
-   // handle functions
+   // Handle functions for redirect and shorten
    http.HandleFunc("/", redirect)
-   http.HandleFunc("/shortened", shorten)
+   http.HandleFunc("/shorten", shorten)
 
-   // creating web server on port 8080
+   // Creating web server on port 8080
    if err := http.ListenAndServe(":8080", nil); err != nil {
        log.Fatal(err) // checking for valid website
-}
+	}
 }
 
+// Unit Test Function for HTTP Handler
+func Teapot(writer http.ResponseWriter, req *http.Request) {
+	writer.WriteHeader(http.StatusTeapot)
+}
+// Unit Test Function for HTTP Handler
+func TestTeapotHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+
+	Teapot(res, req)
+
+	if res.Code != http.StatusTeapot {
+		t.Errorf("got status %d but wanted %d", res.Code, http.StatusTeapot)
+	}
+}
